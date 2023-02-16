1c4d951ecedb727e244773210f29a7bcccda9b51
susheel tiwari <tiwarisusheel2022@gmail.com>
2023-02-16 17:57:40 +0530

    finished gatsby tutoria part-3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,9 @@ module.exports = {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,26 @@
 //step 1 import react
 import * as React from 'react'
 import Layout from '../component/Layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 //Step 2 define your component
 const IndexPage = () => {
   return (
+  <>
   <Layout pageTitle="Home Page">
-  <p>This is my first reusable layout component</p>
-  </Layout>
+      <p>This is my first reusable layout component</p>
+      <StaticImage
+        alt="beautiful butterfly"
+        src="../images/holi.jpg"
+        />
+        <StaticImage
+        alt="Bhole baba"
+        src="../images/Bhola.jpeg"
+        />
+    </Layout>
+    </>
   )
+
 }
 
 //you will learn about this in the next task, just copy it for now
